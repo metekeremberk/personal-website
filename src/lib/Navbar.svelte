a48efc2ee93b720fc48511b02c7d2c010889125f
Mete Kerem Berk <metekeremberk@gmail.com>
2023-02-15 21:51:04 +0300

    Navbar added

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	console.log($page.url.href);
+</script>
+
+<div class="flex flex-row justify-between px-14 py-8 bg-neutral-800 text-white">
+	<div class="text-3xl hover:cursor-default">Logo</div>
+	<nav class="text-xl flex flex-row justify-between w-1/4">
+		<a href="/">Home</a>
+		<a href="/about">About Me</a>
+		<a href="/contacts">My Contacts</a>
+	</nav>
+</div>
