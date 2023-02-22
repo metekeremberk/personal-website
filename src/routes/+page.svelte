70a929e9fb822c2f8aadf1a12c24499349910a15
Mete Kerem Berk <metekeremberk@gmail.com>
2023-02-23 00:48:38 +0300

    scroll bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { scrollTo, scrollRef } from 'svelte-scrolling';
+
 	let scrollY = 0;
 	let innerHeight = 0;
 	let clientHeight = 0;
@@ -14,29 +16,32 @@
 	bind:clientHeight
 	style="--pattern-y: {scrollPerc}px; --pattern-x: {scrollPercx}%;"
 >
-	<nav
-		class="fixed right-10 top-[30%] bg-homeBackgroundSecondary h-[40vh] w-1 rounded-full flex flex-col items-center justify-around z-50"
+	<div
+		class="fixed right-10 top-[30%] bg-homeBackgroundSecondary h-[40vh] w-1 py-10 rounded-full flex flex-col items-center justify-around z-50"
 	>
-		<a href="/" class="w-3 h-3 rounded-full bg-white"><span /></a>
-		<a href="/"><span /></a>
-		<a href="/"><span /></a>
-		<a href="/"><span /></a>
-		<a href="/"><span /></a>
-	</nav>
-	<div class="w-screen h-[120vh] bg-homeBackground">
+		<div use:scrollTo={{ ref: 'home', duration: 1000 }} class="w-3 h-3 rounded-full bg-white" />
+		<div use:scrollTo={{ ref: 'about-1', duration: 1000 }} class="w-3 h-3 rounded-full bg-white" />
+		<div use:scrollTo={{ ref: 'about-2', duration: 1000 }} class="w-3 h-3 rounded-full bg-white" />
+		<div use:scrollTo={{ ref: 'scroll', duration: 1000 }} class="w-3 h-3 rounded-full bg-white" />
+		<div use:scrollTo={{ ref: 'home', duration: 1000 }} class="w-3 h-3 rounded-full bg-white" />
+	</div>
+	<div use:scrollRef={'home'} class="w-screen h-[120vh] bg-homeBackground">
 		<img
-			src="src/lib/background.jpg"
+			src="src/assets/background.jpg"
 			alt="background"
 			class="object-cover w-screen h-[120vh] opacity-30"
 		/>
 		<h1 class="absolute top-[75vh] left-1/2 -translate-x-1/2 text-7xl font-bold">metekeremberk</h1>
 	</div>
-	<div class="w-screen h-[120vh] flex flex-col items-center justify-evenly">
+	<div
+		use:scrollRef={'about-1'}
+		class="w-screen h-screen my-[10vh] flex flex-col items-center justify-evenly"
+	>
 		<div
 			class="flex flex-col items-center justify-evenly w-[400px] m-auto p-6 bg-homeBackgroundSecondary border-4 border-homeText"
 		>
 			<img
-				src="src/lib/about-1.jpg"
+				src="src/assets/about-1.jpg"
 				alt="study"
 				class="object-cover border-homeText border-4 mb-10"
 			/>
@@ -53,12 +58,15 @@
 			</div>
 		</div>
 	</div>
-	<div class="h-[120vh] w-screen flex flex-col items-center justify-evenly">
+	<div
+		use:scrollRef={'about-2'}
+		class="h-screen my-[10vh] w-screen flex flex-col items-center justify-evenly"
+	>
 		<div
 			class="flex flex-col items-center justify-evenly w-[400px] m-auto p-6 bg-homeBackgroundSecondary border-4 border-homeText"
 		>
 			<img
-				src="src/lib/about-2.jpg"
+				src="src/assets/about-2.jpg"
 				alt="hobbies"
 				class="object-cover border-homeText border-4 mb-10"
 			/>
@@ -68,7 +76,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="w-screen h-[110vh] flex items-end">
+	<div use:scrollRef={'scroll'} class="w-screen h-screen my-[10vh] flex items-end">
 		You have scrolled {scrollY} out of {clientHeight}
 	</div>
 </div>
